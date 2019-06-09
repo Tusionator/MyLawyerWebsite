879d9f8f4c9e10e7815e6a2a8c60d45b3e3eaf19
Tusionator <marta.kowalczyk.93@gmail.com>
2019-06-09 17:30:49 +0200

    flipping realm in progress

--- realms/realms.js
+++ realms/realms.js
@@ -11,24 +11,23 @@ var Realm = {
     },
     template:
         `
-      <button class="realm">
-          <div class="realm-main">
-              <h1>{{title}}</h1>
-              <div class="realm-img">
-                  <img style="width: auto; height: 100%;"
-                  v-bind:src="image">
-              </div>
-          </div>
-          <div class="realm-description">
-          <ul>
 
-              <li v-for="item in tmp">
-                   {{ item }}<br>
-              </li>
-              </ul>
+<button class="col-12 col-sm-6 col-lg-4 realm ">
+    <div class="realm-main">
+        <div class="realm-front">
+            <h1>{{title}}</h1>
+        </div>
+        <div class="realm-description">
+            <h1>{{title}}</h1>
+            <ul>
+                <li v-for="item in tmp">
+                    {{ item }}<br>
+                </li>
+            </ul>
+        </div>
+    </div>
 
-          </div>
-      </button>
+</button>
   `
 };
 new Vue({
